b7b8b215f499ef00e5dca2630084448204c64c80
juliakharybina <juliakharybina@gmail.com>
2019-11-24 23:00:07 +0100

    Create WordService
    Created WordService(class) that allows to insert words into DB

---
+++ WordsLearner/Services/WordService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using WindowsFormsApplication1;
+
+public class WordService
+{
+    private DBClassesDataContext dataContext; //object is created to represent our database
+
+    public WordService(DBClassesDataContext dataContext) //constructor
+    {
+        this.dataContext = dataContext;
+    }
+
+    public ICollection<Word> getAll() //to get all_words
+    {
+        return dataContext.Words.ToList();
+    }
+
+    public void create(Word record)
+    {
+        dataContext.Words.InsertOnSubmit(record);
+        dataContext.SubmitChanges();
+    }
+
+    public void batchCreate(IList<Word> wordList)
+    {
+        dataContext.Words.InsertAllOnSubmit(wordList); //we add information to the table
+        dataContext.SubmitChanges(); //to execute a sql insert
+    }
+
+    public bool checkIfWordsExistInDb(String word1, String word2) //cheking if words exist in our database
+    {
+        return dataContext.Words.Where(w => w.word1 == word1 || w.word1 == word2).ToList().Count() == 0;
+    }
+	
+	//lower functions are needed for further work
+    public ICollection<Word> getWordsByLanguageId(long id)
+    {
+        return dataContext.Words.Where(w => w.language_id == id).ToList();
+    }
+
+    public String getWordById(int id)
+    {
+        return dataContext.Words.Where(w => w.Id == id).ToList().First().word1;
+    }
+    public int getIdByWord(string word1)
+    {
+        return dataContext.Words.Where(w => w.word1 == word1).ToList().First().Id;
+    }
+    public int getIdLanguageByWordId(int word1ID)
+    {
+        return dataContext.Words.Where(w => w.Id == word1ID).ToList().First().language_id;
+    }
+    public int getIdLanguageByWord(string word)
+    {
+        return dataContext.Words.Where(w => w.word1 == word).ToList().First().language_id;
+    }
+}
+
